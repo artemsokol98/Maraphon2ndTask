49d02e3e5fea1761e7cb5ced13951f49be0af11c
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-07-04 19:35:57 +0300

    Initial Commit

---
+++ Maraphon2ndTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maraphon2ndTask
+//
+//  Created by Артем Соколовский on 04.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
